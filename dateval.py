cb42e078d2102d9e9a894d6e751ea5dbe4c54660
Harald von Waldow <harald.vonwaldow@eawag.ch>
2015-12-07 22:15:27 +0100

    tests

--- dateval.py
+++ dateval.py
@@ -1,34 +1,53 @@
+from ckan.plugins.toolkit import Invalid
 import re
 
-def solr_datetime_eval(datestr):
+class SolrDaterange(object):
     '''Evaluates wheter a date-time string conforms to
-    the DateRangeField specification of SOLR 5.4. Only ranges implicitly
-    formulated as truncated dates are considered at the moment.
+    the DateRangeField specification of SOLR 5.4.
     https://cwiki.apache.org/confluence/display/solr/Working+with+Dates
     '''
-
-    dateregex = '^(?P<year>-?\d{4,})(-(?P<month>\d{2})(-(?P<day>\d{2}))?)?$'
-    timeregex = '^(?P<hours>\d{2})(:(?P<mins>\d{2})(:(?P<secs>\d{2}(\.\d+)?Z))?)?$'
     
-    def split_dt(datestr):
+    dateregex = '^(?P<year>-?\d{4,})(-(?P<month>\d{2})(-(?P<day>\d{2}))?)?$|^(?P<wildcard>\*)$'
+    timeregex = '^(?P<hours>\d{2})(:(?P<mins>\d{2})(:(?P<secs>\d{2}(\.\d+)?Z))?)?$'
+    expli_dateregex = '^(?P<start>.*) TO (?P<end>.*)$'
+
+    @classmethod
+    def _split_explicit_range(cls, datestr):
+        ''' Splits explicit range (containing "TO") into two
+        implicit range fields'''
+        m = re.match(cls.expli_dateregex, datestr)
+        if m:
+            return((m.groupdict()['start'], m.groupdict()['end']))
+        else:
+            return((datestr,))
+
+    @classmethod
+    def _split_dt(cls, datestr):
         try:
             dat, tim = datestr.split('T')
+            if tim == '':
+                raise Invalid("{} is not a valid date".format(datestr))
         except ValueError:
             dat = datestr.split('T')[0]
             tim = ''
         return((dat, tim))
 
-    def valdate(dat):
-        m = re.match(dateregex, dat)
+    @classmethod
+    def _val_date(cls, dat):
+        m = re.match(cls.dateregex, dat)
         if not m:
-            raise ValueError("{} is not a valid date".format(dat))
+            raise Invalid("{} is not a valid date".format(dat))
+        print("GROUPDICT: {}".format(m.groupdict()))
         return(m.groupdict())
 
-    def val_month_day(datedict):
+    @classmethod
+    def _val_month_day(cls, datedict):
+        print("DATEDICT {}".format(datedict))
+        if datedict['wildcard'] == '*':
+            return()
         intyear = int(datedict['year'])
         try:
             intmonth = int(datedict['month'])
-            # print("{} -> {}".format(datedict['month'], intmonth))
         except TypeError:
             intmonth = None
         try:
@@ -37,23 +56,26 @@ def solr_datetime_eval(datestr):
             intday = None
         if ((datedict['year'].startswith('0') and len(datedict['year'])) > 4 or
             (datedict['year'].startswith('-0') and len(datedict['year'])) > 5):
-            raise ValueError("{} is not a valid year.".format(datedict['year']))
+            raise Invalid("{} is not a valid year.".format(datedict['year']))
         noleapyear = intyear % 4 != 0 or (intyear % 100 == 0
                                           and intyear % 400 != 0)
         maxdays = [31, 28 if noleapyear else 29, 31, 30, 31, 30, 31, 31, 30, 31,
                    30, 31]
         if not (intmonth is None or 1 <= intmonth <= 12):
-            raise ValueError("{} is not a valid month.".format(datedict['month']))
+            raise Invalid("{} is not a valid month.".format(datedict['month']))
         if not (intday is None or 1 <= intday <= maxdays[intmonth - 1]):
-            raise ValueError("{} is not a valid day.".format(datedict['day']))
-
-    def valtime(tim):
-        m = re.match(timeregex, tim)
+            raise Invalid("{} is not a valid day.".format(datedict['day']))
+        
+    @classmethod
+    def _valtime(cls, tim):
+        print("TIM: {}".format(tim))
+        m = re.match(cls.timeregex, tim)
         if not m:
-            raise ValueError("{} is not a valid time.".format(tim))
+            raise Invalid("{} is not a valid time.".format(tim))
         return(m.groupdict())
     
-    def valhour_min_sec(timedict):
+    @classmethod
+    def _valhour_min_sec(cls, timedict):
         inthour = int(timedict["hours"])
         try:
             intmins = int(timedict["mins"])
@@ -66,48 +88,21 @@ def solr_datetime_eval(datestr):
             intsecs = float(timedict["secs"][0:-1])
         if inthour == 24 and ((intmins > 0 and not intmins is None) or
                               (intsecs > 0 and not intsecs is None)):
-            raise ValueError("{} is not a valid hour".format(timedict["hours"]))
+            raise Invalid("{} is not a valid hour".format(timedict["hours"]))
         if inthour >= 25:
-            raise ValueError("{} is not a valid hour".format(timedict["hours"]))
+            raise Invalid("{} is not a valid hour".format(timedict["hours"]))
         if intmins is not None and not (0 <= intmins <= 59):
-            raise ValueError("{} are not valid minutes".format(timedict["mins"]))
+            raise Invalid("{} are not valid minutes".format(timedict["mins"]))
         if intsecs is not None and not (0 <= intsecs < 60.):
-            raise ValueError("{} are not valid seconds".format(timedict["secs"]))
+            raise Invalid("{} are not valid seconds".format(timedict["secs"]))
         return()
  
-
-
-    dat, tim = split_dt(datestr)
-    val_month_day(valdate(dat))
-    if tim != '':
-        valhour_min_sec(valtime(tim))
-
-
-############# Testing
-
-timetest = ['', '0', '02:', ':04', '25', '02:2', '03:60',
-    '03:59:','03:59:3Z', '04:48:967Z', '07:32:59.', '15:44:49.98',
-    '01:00', '00','15:44:49.98Z', '24:07:00Z', '24:00:00.005Z',
-    '04', '05:12', '17:54:12Z', '24', '24:00:00Z', '23:12:18.986754Z']
-timetest = ['2015-11-30T' + x for x in timetest]
-
-for t in timetest:
-    try:
-        solr_datetime_eval(t)
-        print("{} -> OK".format(t))
-    except ValueError as e:
-        print("{} -> {}".format(t, e))
-
-
-datetest = ['2014-02-29', '2300-02-29',  '01232-01-31',
-            '-08765-12-31', '2016-32-03', '-987658-00-07', '87654-08-00',
-            '2400-02-29', '2014-02-28', '0023', '0435-12', '-0987-05-31']
-datetest = [x + 'T23:59:58.9997Z' for x in datetest]
-
-for d in datetest:
-    try:
-        datedict = solr_datetime_eval(d)
-        print("{} -> OK".format(d))
-    except ValueError as e:
-        print("{} -> {}".format(d, e.message))
-
+    @classmethod
+    def validate(cls, datestr):
+        dates = cls._split_explicit_range(datestr)
+        for timestamp in dates:
+            dat, tim = cls._split_dt(timestamp)
+            print("DAT: {}".format(dat))
+            cls._val_month_day(cls._val_date(dat))
+            if tim:
+                cls._valhour_min_sec(cls._valtime(tim))
